89ce0a6fe5631cf4517d927307368f78ac8fc4fe
Federico <federicoc03@gmail.com>
2023-07-24 18:55:06 -0500

    chore: :construction: setting-landPage

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <>
+      <h2>Hola</h2>
+      <h2>Mundo</h2>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header/Header";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      {/* <h2>Esto es el Home</h2> */}
+    </>
+  );
+}
+
+export default Home;
