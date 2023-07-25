80d130bdae3f6981a4e5f31560d8c0e686e941c1
Federico <federicoc03@gmail.com>
2023-07-24 19:21:53 -0500

    feat: :rotating_light: add-Linter-editorConfig-Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import PeerToPeer from "./components/PeerToPeer";
-import Home from "./pages/Home";
-import "./App.css";
+import PeerToPeer from './components/PeerToPeer';
+import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
     <>
       <Home />
+      <h2>.</h2>
     </>
   );
 }
